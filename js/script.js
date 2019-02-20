35022759103c8a735413a321b872a3912600d2f6
Adel <htwr86@gmail.com>
2019-02-20 18:57:57 +0300

    second HW

--- js/script.js
+++ js/script.js
@@ -30,8 +30,11 @@ goodsBtn.forEach(function(btn, i){
             empty = cartWrapper.querySelector('.empty');
         
         trigger.remove();
+
+        showConfirm();
+        calcGoods(1);
         
-        // removeBtn.classList.add('goods__item-remove');
+        removeBtn.classList.add('goods__item-remove');
         removeBtn.innerHTML = '&times';
         item.appendChild(removeBtn);
 
@@ -39,5 +42,72 @@ goodsBtn.forEach(function(btn, i){
         if (empty) {
             empty.remove();
         } 
+
+        calcTotal();
+        removeFromCart();
     }); 
-});
+}); 
+
+
+
+function sliceTitle() {
+   titles.forEach(function(item) {
+    if (item.textContent.length < 70) {
+         return;
+       } else {
+            const str = `${item.textContent.slice(0,71)} ...'`;
+            item.textContent = str;
+       }
+
+    }) 
+}
+sliceTitle();
+
+function showConfirm() {
+    confirm.style.display = 'block';
+    let counter = 100;
+    const id = setInterval(frame, 10);
+    function frame() {
+        if ( counter == 10) {
+            clearInterval(id);
+            confirm.style.display = 'none';
+        } else {
+        counter--;
+        confirm.style.opacity = '.' + counter;
+        confirm.style.transform = `translateY(-${counter}px)`;
+        }
+    }
+}
+
+function calcGoods(i) {
+    const items = cartWrapper.querySelectorAll('.goods__item');
+    badge.textContent = i + items.length;
+    return(items.length);
+}
+
+function calcTotal() {
+    const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+    let total = 0;
+    prices.forEach(function(item) {
+        total += +item.textContent;
+    })
+    totalCost.textContent = total;
+}
+
+function removeFromCart() {
+    const empty = cartWrapper.querySelector('.empty');
+    const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+    removeBtn.forEach(function(btn) {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            calcGoods(0);
+            calcTotal();
+            if (calcGoods() == 0) {
+                empty.style.display = 'block';
+            }
+        });
+    });
+  
+}
+//setInterval(sliceTitle, 100)
+//setTimeout(sliceTitle, 100)
